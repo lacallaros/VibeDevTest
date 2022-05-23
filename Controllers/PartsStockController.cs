417b7596f06d7502cfa6c3f41d74528a15f9b1d1
lacallaros <shlkapcio@gmail.com>
2022-05-23 23:42:29 +0200

    Added communication with SQLEXPRESS and implemented base methods

--- Controllers/PartsStockController.cs
+++ Controllers/PartsStockController.cs
@@ -7,5 +7,72 @@ namespace VibeDevTest.Controllers
     [ApiController]
     public class PartsStockController : ControllerBase
     {
+        private readonly DataContext context;
+
+        public PartsStockController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PartsStock>>> Get()
+        {
+            return Ok(await context.PartsStocks.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PartsStock>> Get(int id)
+        {
+            var part = await context.PartsStocks.FindAsync(id);
+            if (part == null)
+            {
+                return BadRequest("Part not found.");
+            }
+            
+            return Ok(part);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<PartsStock>>> AddPart(PartsStock part)
+        {
+            context.PartsStocks.Add(part);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.PartsStocks.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<PartsStock>>> UpdatePart(PartsStock request)
+        {
+            var dbPart = await context.PartsStocks.FindAsync(request.Id);
+            if (dbPart == null)
+            {
+                return BadRequest("Part not found.");
+            }
+
+            dbPart.Name = request.Name;
+            dbPart.Price = request.Price;
+            dbPart.Availability = request.Availability;
+            dbPart.Quantity = request.Quantity;
+
+            await context.SaveChangesAsync();
+
+            return Ok(await context.PartsStocks.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<PartsStock>>> DeletePart(int id)
+        {
+            var dbPart = await context.PartsStocks.FindAsync(id);
+            if (dbPart == null)
+            {
+                return BadRequest("Part not found.");
+            }
+            
+            context.PartsStocks.Remove(dbPart);
+            await context.SaveChangesAsync();
+
+            return Ok(await context.PartsStocks.ToListAsync());
+        }
     }
 }
