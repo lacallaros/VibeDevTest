b8708bd3c6b656da16812b32ba0fe0225ba9a4e2
lacallaros <shlkapcio@gmail.com>
2022-05-30 19:37:38 +0200

    Fixes

--- Dto/PartsStockDto.cs
+++ Dto/PartsStockDto.cs
@@ -5,21 +5,17 @@ namespace VibeDevTest.Dto
 {
     public class PartsStockDto
     {
-        public int Id { get; set; }
         [Required]
         [StringLength(50)]
         public string Name { get; set; }
         [Required] 
         public string Price { get; set; }
-        public int Quantity { get; set; }
         public bool Availability { get; set; }
 
         public PartsStockDto(PartsStock p)
         {
-            Id = p.Id;
             Name = p.Name;
             Price = p.Price;
-            Quantity = p.Quantity;
             Availability = p.Availability;
         }
     }
